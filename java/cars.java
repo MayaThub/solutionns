25510f865f5890e8af5cbdb239f4c25a07b91144
ELISETTI VEERA VENKATA GANESH <138519420+ganesh714@users.noreply.github.com>
2025-07-04 23:18:05 +0530

    Create cars.java

---
+++ java/cars.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        System.out.println((int)Math.ceil(n/4.0));
+    }
+}
