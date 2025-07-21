9aaf21c73eb738eef421882b4d4c144a8cec74bc
ELISETTI VEERA VENKATA GANESH <138519420+ganesh714@users.noreply.github.com>
2025-07-21 18:43:26 +0530

    Create reach_home.java

---
+++ java/reach_home.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Sol{
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        if (x*5 >= y) {
+            System.out.println("YES");
+        }
+        else {
+            System.out.println("NO");
+        }
+    }
+}
