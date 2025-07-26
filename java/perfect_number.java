85df6a889be9f6bce45c28286b6638338fc08858
ELISETTI VEERA VENKATA GANESH <138519420+ganesh714@users.noreply.github.com>
2025-07-26 14:14:10 +0530

    Create perfect_number.java

---
+++ java/perfect_number.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Sol{
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 1;
+
+        for (int i = 2; i <= Math.ceil(n/2); i++) {
+            if (n % i == 0)
+                sum += i;
+        }
+        if (sum == n)
+            System.out.println("True");
+        else
+            System.out.println("False");
+    }
+}
